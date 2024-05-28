330c1ba402cd6b6fb783ac15c038595240653741
Ariagny <ariagnyborregaleso1@gmail.com>
2024-05-28 13:37:55 -0500

    Ejercicios

---
+++ 06-DOM/Práctica/app.js
@@ -0,0 +1,26 @@
+//Ejercicio del boton 
+const miImput = document.getElementById("boton");
+
+function cambiarValue(){
+    console.log(miImput.value)
+    miImput.value = "lanzamiento exitoso 😁"
+}
+
+miImput.addEventListener("click", cambiarValue);
+
+//ejercicio del cuadro de colores 
+const cajaColor = document.getElementById("caja");
+const botonesColores = document.querySelectorAll(".btnColores");
+
+console.log(cajaColor)
+console.log(botonesColores)
+
+function pintarCaja (e){
+    console.log(e.target.getAttribute("data-color"))
+    let newColor = e.target.getAttribute("data-color");
+    cajaColor.style.backgroundColor = newColor;
+}
+botonesColores[0].addEventListener("click",pintarCaja)
+botonesColores[1].addEventListener("click",pintarCaja)
+botonesColores[2].addEventListener("click",pintarCaja)
+botonesColores[3].addEventListener("click",pintarCaja)
