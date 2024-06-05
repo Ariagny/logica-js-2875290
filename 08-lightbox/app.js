0c02a3b1d816a5404acd2b2a44ab4513e15b7450
Ariagny <ariagnyborregaleso1@gmail.com>
2024-06-05 13:29:09 -0500

    Ejercicio

---
+++ 08-lightbox/app.js
@@ -0,0 +1,32 @@
+const btnNumeros  = document.querySelectorAll(".min");
+const flotante = document.querySelector("#flotante");
+const cuadroOpacidad = document.querySelector("#cuadro");
+const equis = document.querySelector("#equis");
+const btnSiguiente = document.querySelector("#sig");
+const btnAnterior = document.querySelector("#ant");
+const imgPrincipal = document.querySelector("#imgprincipal");
+
+//funciones
+function lightbox (e){
+    console.log(e.target.src)
+    let currentImg = e.target.src;
+    flotante.style.top = "0vh";
+    flotante.style.transitionDuration = "0.5s";
+    imgPrincipal.setAttribute("src",currentImg)
+}
+
+function cerrar (){
+    flotante.style.top = "-100vh";
+    flotante.style.transitionDuration = "0.5s";
+
+}
+
+function cambiar (){
+
+}
+
+btnNumeros[0].addEventListener("click",lightbox);
+btnNumeros[1].addEventListener("click",lightbox);
+equis.addEventListener("click",cerrar);
+cuadroOpacidad.addEventListener("click",cerrar);
+
